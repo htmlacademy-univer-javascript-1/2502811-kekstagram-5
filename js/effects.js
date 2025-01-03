ceec3cf783c1e45832e4ab49e4adff93f9e3f7bc
DanaLee7 <dianbucha@mail.ru>
2025-01-03 11:40:56 +0500

    Надо подкачаться

--- js/effects.js
+++ js/effects.js
@@ -134,4 +134,5 @@ const initEffect = () => {
   }
 };
 
+
 export {resetEffect, initEffect};
--- js/newPublication.js
+++ js/newPublication.js
@@ -2,6 +2,7 @@ import { onDocumentKeydown } from './util.js';
 import { MAX_HASHTAGS_COUNT, MAX_DESCRIPTION_LENGTH } from './data.js';
 import {resetEffect, initEffect} from './effects.js';
 import {resetScale} from './scale.js';
+import {sendData} from './api.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadInput = uploadForm.querySelector('.img-upload__input');
@@ -27,8 +28,8 @@ const validateHashtagsCount = (value) => value.trim().split(' ').length <= MAX_H
 const validateHashtagsUniqueness = (value) => {
   const hashtags = value.split(' ');
   const hashTagMap = {};
-  for (let i = 0; i < hashtags.length; i++) {
-    const hashtag = hashtags[i];
+  for (const element of hashtags) {
+    const hashtag = element;
     if (hashtag in hashTagMap) {
       return false;
     }
@@ -42,8 +43,8 @@ const validHashtages = (value) => {
     return true;
   }
   const hashtags = value.trim().split(' ');
-  for (let i = 0; i < hashtags.length; ++i) {
-    if (!validationForm.test(hashtags[i])) {
+  for (const element of hashtags) {
+    if (!validationForm.test(element)) {
       return false;
     }
   }
@@ -76,26 +77,32 @@ pristine.addValidator(
   'Описание не может быть больше 140 символов'
 );
 
-function closeOverlay(){
-  imageOverlay.classList.add('hidden');
+function resetForm() {
   resetEffect();
   resetScale();
+  uploadForm.reset();
+}
+
+function closeOverlay(){
+  imageOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
-  closeButton.removeEventListener('click', closeOverlay);
+  closeButton.removeEventListener('click', () => {resetForm(); closeOverlay();});
   document.removeEventListener('keydown', onDocumentKeydown(closeOverlay));
   uploadInput.addEventListener('click', openOverlay);
   uploadInput.value = null;
   hashtagsField.textContent = '';
   descriptionField.textContent = '';
+  submitBtn.removeAttribute('disabled');
 }
 
 function openOverlay() {
   initEffect();
   imageOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
-  closeButton.addEventListener('click', closeOverlay);
+  closeButton.addEventListener('click', () => {resetForm(); closeOverlay();});
   document.addEventListener('keydown', onDocumentKeydown(closeOverlay));
   uploadInput.removeEventListener('click', openOverlay);
+
 }
 
 uploadInput.addEventListener('change', openOverlay);
@@ -121,3 +128,51 @@ descriptionField.addEventListener('input', (evt) => {
     submitBtn.removeAttribute('disabled');
   }
 });
+
+
+uploadForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(uploadForm);
+  submitBtn.setAttribute('disabled', 'true');
+  sendData(
+    formData,
+    () => {
+      closeOverlay();
+      resetForm();
+      openSuccessMessage();
+    },
+    () => {
+      closeOverlay();
+      openErrorMessage();
+    },
+  );
+});
+
+
+function closeSuccessMessage() {
+  document.querySelector('.success').remove();
+  document.removeEventListener('keydown', onDocumentKeydown(closeSuccessMessage));
+}
+
+function openSuccessMessage() {
+  document.body.appendChild(document.querySelector('#success').content.cloneNode(true));
+  const success = document.querySelector('.success');
+  success.children[0].addEventListener('click', (e) => e.stopPropagation());
+  success.addEventListener('click', () => {success.remove();});
+  document.addEventListener('keydown', onDocumentKeydown(closeSuccessMessage), true);
+  success.querySelector('button').addEventListener('click', () => {success.remove();});
+}
+
+function closeErrorMessage() {
+  document.querySelector('.error').remove();
+  document.removeEventListener('keydown', onDocumentKeydown(closeErrorMessage));
+}
+
+function openErrorMessage() {
+  document.body.appendChild(document.querySelector('#error').content.cloneNode(true));
+  const error = document.querySelector('.error');
+  error.children[0].addEventListener('click', (e) => e.stopPropagation());
+  error.addEventListener('click', () => {error.remove();});
+  document.addEventListener('keydown', onDocumentKeydown(closeErrorMessage), true);
+  error.querySelector('button').addEventListener('click', () => {error.remove();});
+}
