d296c18a28dbb80012533e6e19291c5f03c819de
DanaLee7 <dianbucha@mail.ru>
2025-01-03 13:28:28 +0500

    итог

--- js/module.js
+++ js/module.js
@@ -1,14 +1,18 @@
-import { generatePhotoArray } from './data.js';
-import { openPicture } from './openBigPic.js';
+import {getPhotoArray} from './data.js';
+import {openPicture} from './openBigPic.js';
+import {debounce} from './util.js';
 
-export const getRenderedCards = () => {
+const MAX_PHOTOS = 10;
+const DEBOUNCE_TIME = 500;
+
+const renderCards = (posts) => {
+  const picturesContainer = document.querySelector('.pictures');
   const pictureTemplate = document
     .querySelector('#picture')
     .content.querySelector('.picture');
-  const picturesContainer = document.querySelector('.pictures');
-  const randomPosts = generatePhotoArray(25);
+  picturesContainer.querySelectorAll('.picture').forEach((p) => p.remove());
   const picturesFragment = document.createDocumentFragment();
-  randomPosts.forEach(({ url, description, likes, comments }) => {
+  posts.forEach(({url, description, likes, comments}) => {
     const picture = pictureTemplate.cloneNode(true);
     picture.querySelector('img').src = url;
     picture.querySelector('img').alt = description;
@@ -20,4 +24,60 @@ export const getRenderedCards = () => {
     picturesFragment.append(picture);
   });
   picturesContainer.append(picturesFragment);
+};
+
+const toggleActiveClass = (evt) => {
+  const filterButtons = document.querySelectorAll('.img-filters__button');
+  filterButtons.forEach((button) => button.classList.remove('img-filters__button--active'));
+  evt.target.classList.add('img-filters__button--active');
+};
+
+const onFilterDefaultClick = debounce((posts, event) => {
+  toggleActiveClass(event);
+  renderCards(posts);
+}, DEBOUNCE_TIME);
+
+const onFilterRandomClick = debounce((posts, event) => {
+  toggleActiveClass(event);
+  const randomPhotos = posts
+    .toSorted(() => Math.random() - 0.5)
+    .slice(0, MAX_PHOTOS);
+  renderCards(randomPhotos);
+}, DEBOUNCE_TIME);
+
+const onFilterDiscussedClick = debounce((posts, event) => {
+  toggleActiveClass(event);
+  const discussedPhotos = posts.toSorted((a, b) => b.comments.length - a.comments.length);
+  renderCards(discussedPhotos);
+}, DEBOUNCE_TIME);
+
+
+export const renderGallery = async () => {
+  const imgFilters = document.querySelector('.img-filters');
+  const filterDefault = imgFilters.querySelector('#filter-default');
+  const filterRandom = imgFilters.querySelector('#filter-random');
+  const filterDiscussed = imgFilters.querySelector('#filter-discussed');
+  try {
+    const posts = await getPhotoArray();
+    renderCards(posts);
+    imgFilters.classList.remove('img-filters--inactive');
+    filterDefault.addEventListener('click', (e) => onFilterDefaultClick(posts, e));
+    filterRandom.addEventListener('click', (e) => onFilterRandomClick(posts, e));
+    filterDiscussed.addEventListener('click', (e) => onFilterDiscussedClick(posts, e));
+  } catch (err) {
+    const errorBlock = document.createElement('div');
+    const errorMessage = document.createElement('pre');
+    errorBlock.textContent = 'Произошла ошибка при загрузке данных';
+    errorMessage.textContent = err;
+    errorMessage.style.fontSize = '16px';
+    errorBlock.style.fontSize = '20px';
+    errorBlock.style.textAlign = 'center';
+    errorBlock.style.marginTop = '20px';
+    errorBlock.style.border = '1px solid red';
+    errorBlock.style.padding = '10px';
+    errorBlock.style.borderRadius = '10px';
+    errorBlock.style.background = 'rgba(255, 0, 0, 0.25)';
+    errorBlock.append(errorMessage);
+    document.querySelector('.img-upload').append(errorBlock);
+  }
 };
--- js/newPublication.js
+++ js/newPublication.js
@@ -2,11 +2,13 @@ import { onDocumentKeydown } from './util.js';
 import { MAX_HASHTAGS_COUNT, MAX_DESCRIPTION_LENGTH } from './data.js';
 import {resetEffect, initEffect} from './effects.js';
 import {resetScale} from './scale.js';
+import {sendData} from './api.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadInput = uploadForm.querySelector('.img-upload__input');
 const imageOverlay = uploadForm.querySelector('.img-upload__overlay.hidden');
 const closeButton = uploadForm.querySelector('.img-upload__cancel');
+const image = uploadForm.querySelector('.img-upload__preview img');
 const hashtagsField = uploadForm.querySelector('.text__hashtags');
 const descriptionField = uploadForm.querySelector('.text__description');
 const submitBtn = uploadForm.querySelector('#upload-submit');
@@ -22,13 +24,25 @@ const pristine = new Pristine(uploadForm, {
   errorTextClass: 'img-upload__error'
 });
 
+
+uploadInput.addEventListener('change', function() {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      image.src = reader.result;
+    }, false);
+    reader.readAsDataURL(file);
+  }
+});
+
 const validateHashtagsCount = (value) => value.trim().split(' ').length <= MAX_HASHTAGS_COUNT;
 
 const validateHashtagsUniqueness = (value) => {
   const hashtags = value.split(' ');
   const hashTagMap = {};
-  for (let i = 0; i < hashtags.length; i++) {
-    const hashtag = hashtags[i];
+  for (const element of hashtags) {
+    const hashtag = element;
     if (hashtag in hashTagMap) {
       return false;
     }
@@ -42,8 +56,8 @@ const validHashtages = (value) => {
     return true;
   }
   const hashtags = value.trim().split(' ');
-  for (let i = 0; i < hashtags.length; ++i) {
-    if (!validationForm.test(hashtags[i])) {
+  for (const element of hashtags) {
+    if (!validationForm.test(element)) {
       return false;
     }
   }
@@ -76,24 +90,29 @@ pristine.addValidator(
   'Описание не может быть больше 140 символов'
 );
 
-function closeOverlay(){
-  imageOverlay.classList.add('hidden');
+function resetForm() {
   resetEffect();
   resetScale();
+  uploadForm.reset();
+}
+
+function closeOverlay(){
+  imageOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
-  closeButton.removeEventListener('click', closeOverlay);
+  closeButton.removeEventListener('click', () => {resetForm(); closeOverlay();});
   document.removeEventListener('keydown', onDocumentKeydown(closeOverlay));
   uploadInput.addEventListener('click', openOverlay);
   uploadInput.value = null;
   hashtagsField.textContent = '';
   descriptionField.textContent = '';
+  submitBtn.removeAttribute('disabled');
 }
 
 function openOverlay() {
   initEffect();
   imageOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
-  closeButton.addEventListener('click', closeOverlay);
+  closeButton.addEventListener('click', () => {resetForm(); closeOverlay();});
   document.addEventListener('keydown', onDocumentKeydown(closeOverlay));
   uploadInput.removeEventListener('click', openOverlay);
 }
@@ -104,7 +123,7 @@ hashtagsField.addEventListener('input', (evt) => {
   evt.preventDefault();
   const isValid = pristine.validate();
   if (!isValid) {
-    submitBtn.setAttribute('disabled', true);
+    submitBtn.setAttribute('disabled', 'true');
   }
   else{
     submitBtn.removeAttribute('disabled');
@@ -115,9 +134,57 @@ descriptionField.addEventListener('input', (evt) => {
   evt.preventDefault();
   const isValid = pristine.validate();
   if (!isValid) {
-    submitBtn.setAttribute('disabled', true);
+    submitBtn.setAttribute('disabled', 'true');
   }
   else{
     submitBtn.removeAttribute('disabled');
   }
 });
+
+
+uploadForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(uploadForm);
+  submitBtn.setAttribute('disabled', 'true');
+  sendData(
+    formData,
+    () => {
+      closeOverlay();
+      resetForm();
+      openSuccessMessage();
+    },
+    () => {
+      closeOverlay();
+      openErrorMessage();
+    },
+  );
+});
+
+
+function closeSuccessMessage() {
+  document.querySelector('.success').remove();
+  document.removeEventListener('keydown', onDocumentKeydown(closeSuccessMessage));
+}
+
+function openSuccessMessage() {
+  document.body.appendChild(document.querySelector('#success').content.cloneNode(true));
+  const success = document.querySelector('.success');
+  success.children[0].addEventListener('click', (e) => e.stopPropagation());
+  success.addEventListener('click', () => {success.remove();});
+  document.addEventListener('keydown', onDocumentKeydown(closeSuccessMessage), true);
+  success.querySelector('button').addEventListener('click', () => {success.remove();});
+}
+
+function closeErrorMessage() {
+  document.querySelector('.error').remove();
+  document.removeEventListener('keydown', onDocumentKeydown(closeErrorMessage));
+}
+
+function openErrorMessage() {
+  document.body.appendChild(document.querySelector('#error').content.cloneNode(true));
+  const error = document.querySelector('.error');
+  error.children[0].addEventListener('click', (e) => e.stopPropagation());
+  error.addEventListener('click', () => {error.remove();});
+  document.addEventListener('keydown', onDocumentKeydown(closeErrorMessage), true);
+  error.querySelector('button').addEventListener('click', () => {error.remove();});
+}
